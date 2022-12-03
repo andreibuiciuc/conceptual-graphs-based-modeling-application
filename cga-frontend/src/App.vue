1f906fc34ec79a018a99b5f9030afdabde844ad0
andreibuiciuc <abuiciuc33@gmail.com>
2022-12-03 22:40:18 +0200

    removed boilerplate and added sidebar template

--- cga-frontend/src/App.vue
+++ cga-frontend/src/App.vue
@@ -1,85 +1,35 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <v-layout>
+    <v-navigation-drawer expand-on-hover rail>
+      <v-list class="navigation-avatar">
+        <RouterLink to="/">
+          <v-list-item prepend-avatar="/cassandra.png"
+                      title="CGA"
+                      subtitle="Cassandra">
+          </v-list-item>
+        </RouterLink>
+      </v-list>
+      <v-divider />
+      <v-list density="compact" nav class="navigation-list">
+        <RouterLink to="/about">
+          <v-list-item prepend-icon="mdi-family-tree"
+                      title="Data Structure CG Design"
+                      value="dataStructureCG">
+          </v-list-item>
+        </RouterLink>
+        <RouterLink to="/about">
+          <v-list-item prepend-icon="mdi-database-search"
+                      title="Query CG Design"
+                      value="queryCG">
+          </v-list-item>
+        </RouterLink>
+      </v-list>
+    </v-navigation-drawer>
+  </v-layout>
 
   <RouterView />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
