277ac4788ce062a44d713b5bc9e959a6c60e77de
andreibuiciuc <abuiciuc33@gmail.com>
2022-12-03 16:43:14 +0200

    connection to cassandra server running on docker done

--- cga-backend/main.py
+++ cga-backend/main.py
@@ -1,6 +1,49 @@
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from typing import Tuple
+from cassandra.cluster import Cluster, Session, ResultSet
+from cassandra.cluster import NoHostAvailable
+
+HOSTS = ['127.0.0.1']
+DOCKER_EXPOSED_PORT = 9042
+
+
+def log_exception_message(exception_object: Exception, message: str) -> None:
+    """
+    Logs an exception to the standard output, together with a custom message.
+    :parameter exception_object: A given exception
+    :parameter message: A given custom message
+    """
+    print(f"Exception: {exception_object} \n {message}")
+
+
+def connect_to_cassandra_server() -> Tuple[Cluster, Session]:
+    """
+    Connects to a Cassandra server instance.
+    :return: A tuple containing the created Cassandra cluster and session
+    """
+    cluster = Cluster(HOSTS, DOCKER_EXPOSED_PORT)
+    session = cluster.connect()
+    return cluster, session
+
+
+def get_available_keyspaces(session: Session) -> ResultSet:
+    """
+    Retrieves the available keyspaces from the Cassandra server.
+    :param session: The current Cassandra Session
+    :return: A ResultSet containing the available keyspaces from the current Cassandra server.
+    """
+    result_set = session.execute('describe keyspaces')
+    return result_set
 
 
 if __name__ == '__main__':
-    print_hi('Conceptual Graphs Application')
+    
+    try:
+        cassandra_cluster, cassandra_session = connect_to_cassandra_server()
+
+        keyspaces_result_set = get_available_keyspaces(cassandra_session)
+        for keyspace_row in keyspaces_result_set:
+            print(keyspace_row)
+    
+    except NoHostAvailable as exception:
+        log_exception_message(type(exception), "Unable to connect to the Cassandra Server.")
+
