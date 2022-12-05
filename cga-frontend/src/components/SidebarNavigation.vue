a2e8c1d63094209a93a93172f437ee8214645cc1
andreibuiciuc <abuiciuc33@gmail.com>
2022-12-05 21:18:59 +0200

    refined the sidebar component and created a theme css file

--- cga-frontend/src/components/SidebarNavigation.vue
+++ cga-frontend/src/components/SidebarNavigation.vue
@@ -3,27 +3,23 @@
     <v-layout>
         <v-navigation-drawer expand-on-hover rail>
           <v-list class="navigation-avatar">
-            <RouterLink to="/">
-              <v-list-item prepend-avatar="/cassandra.png"
-                          title="CGA"
-                          subtitle="Cassandra">
+            <RouterLink :to="navigationHeader.pathTo">
+              <v-list-item :prepend-avatar="navigationHeader.avatar"
+                           :title="navigationHeader.title"
+                           :subtitle="navigationHeader.subtitle">
               </v-list-item>
             </RouterLink>
           </v-list>
           <v-divider />
-          <v-list density="compact" nav class="navigation-list">
-            <RouterLink to="/data-structure">
-              <v-list-item prepend-icon="mdi-family-tree"
-                          title="Data Structure CG Design"
-                          value="dataStructureCG">
-              </v-list-item>
-            </RouterLink>
-            <RouterLink to="/query">
-              <v-list-item prepend-icon="mdi-database-search"
-                          title="Query CG Design"
-                          value="queryCG">
-              </v-list-item>
-            </RouterLink>
+          <v-list class="navigation-list" density="compact" nav>
+            <template v-for="navigationItem in navigationItems" :key="navigationItem.value">
+              <RouterLink :to="navigationItem.pathTo">
+                <v-list-item :prepend-icon="navigationItem.icon"
+                             :title="navigationItem.title"
+                             :value="navigationItem.value">
+                </v-list-item>
+              </RouterLink>
+            </template>
           </v-list>
         </v-navigation-drawer>
       </v-layout>
@@ -32,12 +28,42 @@
 
 <script>
 export default {
-    name: "SidebarNavigation"
+    name: "SidebarNavigation",
+    data: function () {
+      return {
+        title: "CGA",
+        subtitle: "Cassandra",
+        navigationHeader: {
+          title: "CGA",
+          subtitle: "Cassandra",
+          avatar: "/cassandra.png",
+          pathTo: "/"
+        },
+        navigationItems: {
+          dataStructureDesign: {
+            title: "Data Structure CG Design",
+            value: "dataStructure",
+            icon: "mdi-family-tree",
+            pathTo: "/data-structure"
+          },
+          queryDesign: {
+            title: "Query CG Design",
+            value: "query",
+            icon: "mdi-database-search",
+            pathTo: "/query"
+          }
+        }
+      }
+    }
 };
 </script>
 
 <style scoped>
 .v-list-item {
-  color: black;
+  color: var(--cassandra-gray);
+}
+
+.v-list-item--active {
+  color: var(--cassandra-blue) !important;
 }
 </style>
