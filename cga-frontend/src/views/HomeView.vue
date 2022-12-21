189725381c80f7a962496f14fce47dad7080dd06
andreibuiciuc <abuiciuc33@gmail.com>
2022-12-21 18:45:18 +0200

    continued working on the home view components

--- cga-frontend/src/views/HomeView.vue
+++ cga-frontend/src/views/HomeView.vue
@@ -1,11 +1,28 @@
 <template>
   <div class="homeview-body">
-    <!-- TODO: Landing section -->
+
     <section class="landing-section">
       <div class="landing-section-content">
-        <h1>Conceptual Graphs based-modeling Application</h1>
-        <h2>Interface for modeling Cassandra data structure and querying
-          data, based on Conceptual Graphs</h2>
+        <div>
+          <span style="font-size: 60px;"> CGA</span>
+          <h2 style="font-size: 20px"><strong>Conceptual Graphs</strong> based-modeling <strong>Application</strong>: an interface for 
+            modeling Cassandra data structure and querying data, based on Conceptual Graphs</h2>
+        </div>
+      </div>
+      <div class="landing-terminal-wrapper">
+        <div class="landing-terminal">
+          <div class="command-line">
+            <pre>cqlsh >> DESCRIBE keyspaces;</pre>
+            <pre>cqlsh >> TODO: response</pre>
+            <pre>cqlsh >> USE daily_weather_measurements;</pre>
+            <pre>cqlsh >> SELECT m_date , city , station_code, min_temp , max_temp</pre>
+            <pre>      >> FROM daily_weather_measurements</pre>
+            <pre>      >> WHERE m_date ='2020-01-12'</pre>
+            <pre>      >> ORDER BY city ASC;</pre> 
+            <pre class="blip">|</pre>
+          </div>
+        </div>
+        <img src="/cassandra2.png" width="320" height="215"/>
       </div>
     </section>
     
@@ -39,6 +56,28 @@
 <script>
 export default {
   name: "HomeView",
+  data: () => {
+    return {
+      sections: [
+        {
+          key: "landingSection",
+          title: "Home",
+          dotColor: "#bbe6fb",
+          icon: 'home'
+        },
+        {
+          key: "summarySection",
+          title: "Summary",
+          dotColor: "#1287b1"
+        },
+        {
+          key: "authSection",
+          title: "Authentication",
+          dotColor: "#373535"
+        }
+      ]
+    };
+  }
 }
 </script>
 
@@ -59,8 +98,48 @@ section {
 
 /* Style related to the landing section */
 .landing-section {
+  flex-direction: row;
+  justify-content: flex-start;
   min-height: 50vh;
   background-color: var(--cassandra-white);
+}
+
+.landing-terminal-wrapper {
+  position: relative;
+  background-image: url('../assets/svg/triangle-lines.svg');
+}
+
+.landing-terminal {
+  width: 600px;
+  height: 300px;
+  background-color: var(--cassandra-black);
+  border-radius: 10px;
+  padding: 1rem;
+}
+
+.command-line {
+  color: var(--cassandra-white);
+}
+
+img {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+}
+
+@keyframes blipping {
+  from {
+    color: var(--cassandra-white);
+  }
+  to {
+    color: var(--cassandra-black);
+  }
+}
+
+.blip {
+  animation-name: blipping;
+  animation-duration: 1s;
+  animation-iteration-count: infinite;
 }
 
 .homeview-body > .landing-section > .landing-section-content h1 {
