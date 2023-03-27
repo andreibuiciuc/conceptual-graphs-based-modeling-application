b6420681ad8abacb076019cc67344c65b277294e
Andrei Buiciuc <abuiciuc33@gmail.com>
2023-03-27 23:33:34 +0300

    done main layout for query view

--- cga-frontend/src/views/QueryView.vue
+++ cga-frontend/src/views/QueryView.vue
@@ -1,6 +1,36 @@
 <template>
-  <div class="flex">
-    <h1>Query Conceptual Graph Design</h1>
+  <div class="query-section">
+    <div class="query-header-container elevation-1">
+      <div>
+        <span>Query Design</span>
+      </div>
+      <div class="query-header-actions">
+        <v-btn variant="outline">
+          Save
+        </v-btn>
+        <v-btn variant="outline">
+          Generate Query
+        </v-btn>
+      </div>
+    </div>
+    <div class="query-canvas-wrapper">
+      <div class="query-canvas">
+        <div class="query-graph-container">
+
+        </div>
+        <div class="query-graph-container">
+
+        </div>
+      </div>
+    </div>
+    <div class="query-toolbox">
+      <v-expansion-panels variant="accordion">
+        <v-expansion-panel
+          title="Item"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        ></v-expansion-panel>
+      </v-expansion-panels>
+    </div>
   </div>
 </template>
 
@@ -21,9 +51,36 @@ export default {
 </script>
 
 <style scoped lang="sass">
+@use "@/assets/styles/_variables.sass"
 @use "@/assets/styles/_containers.sass"
 
-.flex
-  @include containers.flex-container($justify-content: center, $align-items: center)
-  height: 100vh
+.query-section
+  @include containers.flex-container($flex-direction: column)
+  height: calc(100vh - variables.$cga-topbar-height)
+  margin-top: variables.$cga-topbar-height
+
+  .query-header-container
+    @include containers.flex-container($flex-direction: row, $justify-content: space-between, $align-items: center)
+    height: variables.$cga-header-height
+    min-height: variables.$cga-header-height
+    width: 100%
+    padding: 10px 26px
+
+    .query-header-actions
+      @include containers.flex-container($flex-direction: row, $justify-content: flex-end, $align-items: center)
+
+  .query-canvas-wrapper
+    padding: 10px
+    height: 100%
+    width: 100%
+
+    .query-canvas
+      @include containers.flex-container($flex-direction: row)
+      border: 1px solid variables.$cassandra-light-gray
+      height: 100%
+
+  .query-toolbox
+    width: 100%
+    padding: 10px
+
 </style>
