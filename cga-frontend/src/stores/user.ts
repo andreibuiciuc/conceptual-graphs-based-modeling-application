1edda890c0cea325443f641dea8fa5cd4727a2b3
Andrei Buiciuc <abuiciuc33@gmail.com>
2023-04-13 16:23:51 +0300

    removed comments and added some return types

--- cga-frontend/src/stores/user.ts
+++ cga-frontend/src/stores/user.ts
@@ -10,7 +10,7 @@ export const useUserStore = defineStore('user', () => {
 
     const { openNotificationToast } = useUtils();
 
-    async function register (registerCredentials: RegisterCredentials) {
+    async function register (registerCredentials: RegisterCredentials): Promise<void> {
         const userCredentials = await auth.createUserWithEmailAndPassword(registerCredentials.email, registerCredentials.password);
         await usersCollection.doc(userCredentials.user?.uid).set({
             firstname: registerCredentials.firstname,
@@ -20,7 +20,7 @@ export const useUserStore = defineStore('user', () => {
         isUserLoggedIn.value = true;
     }
 
-    async function login (loginCredentials: LoginCredentials) {
+    async function login (loginCredentials: LoginCredentials): Promise<void> {
         await auth.signInWithEmailAndPassword(loginCredentials.email, loginCredentials.password);
         const userDocumentReference = usersCollection.doc(auth.currentUser?.uid);
         const userDocument = await userDocumentReference.get();
@@ -32,7 +32,7 @@ export const useUserStore = defineStore('user', () => {
         }
     }
 
-    async function signOut () {
+    async function signOut (): Promise<void> {
         await auth.signOut();
         isUserLoggedIn.value = false;
     }
@@ -44,44 +44,3 @@ export const useUserStore = defineStore('user', () => {
         signOut
     };
 });
-
-// export default defineStore("user", {
-//   state: () => ({
-//     isUserLoggedIn: false,
-//     userCredentials: null,
-//   }),
-//   actions: {
-//     // Register the user by email and password using firebase sdk
-//     register: async function (registerCredentials) {
-//       const userCredentials = await auth.createUserWithEmailAndPassword(
-//         registerCredentials.email,
-//         registerCredentials.password
-//       );
-//       await usersCollection.doc(userCredentials.user.uid).set({
-//         firstname: registerCredentials.firstname,
-//         lastname: registerCredentials.lastname,
-//         email: registerCredentials.email,
-//       });
-//       this.isUserLoggedIn = true;
-//     },
-//     // Sign in the user by email and password using firebase sdk
-//     login: async function (loginCredentials) {
-//       await auth.signInWithEmailAndPassword(
-//         loginCredentials.email,
-//         loginCredentials.password
-//       );
-//       const userDocumentRef = usersCollection.doc(auth.currentUser.uid);
-//       userDocumentRef.get().then((userDocument) => {
-//         if (userDocument.exists) {
-//           this.userCredentials = { ...userDocument.data() };
-//         }
-//       });
-//       this.isUserLoggedIn = true;
-//     },
-//     // Logout the user using firebase sdk
-//     signOut: async function () {
-//       await auth.signOut();
-//       this.isUserLoggedIn = false;
-//     },
-//   },
-// });
