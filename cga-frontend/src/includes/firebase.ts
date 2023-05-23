ace837111cba2bc434d74a99d8b112f3669bacea
Andrei Buiciuc <abuiciuc33@gmail.com>
2023-05-23 13:12:38 +0300

    moved firebase configuration to .env file

--- cga-frontend/src/includes/firebase.ts
+++ cga-frontend/src/includes/firebase.ts
@@ -3,21 +3,27 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyByUnJWH5SwOpq_fZKAx1bUR2C6qeFiFZA",
-  authDomain: "cassandra-cga.firebaseapp.com",
-  projectId: "cassandra-cga",
-  storageBucket: "cassandra-cga.appspot.com",
-  messagingSenderId: "931674576947",
-  appId: "1:931674576947:web:59aa32b8fc3e0436926b46",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+
+const firebaseCollections = {
+  USERS: 'users',
+  CONCEPTUAL_GRAPHS: 'conceptual-graphs',
+  TABLE_GRAPHS: 'table-graphs'
 };
 
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const database = firebase.firestore();
-const usersCollection = database.collection("users");
-const conceptualGraphsCollection = database.collection("conceptual-graphs");
-const tableGraphsCollection = database.collection('table-graphs');
+const usersCollection = database.collection(firebaseCollections.USERS);
+const conceptualGraphsCollection = database.collection(firebaseCollections.CONCEPTUAL_GRAPHS);
+const tableGraphsCollection = database.collection(firebaseCollections.TABLE_GRAPHS);
 
 export { 
   auth,
